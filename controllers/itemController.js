ff8d36414564024923054cff3c598330eb9f38ac
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-12-08 15:03:07 -0500

    https://github.com/pmocal/imaginaryinventory/issues/1

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -7,8 +7,35 @@ exports.index = function(req, res) {
 	async.parallel({
 		item_count: function(callback) {
 			Item.countDocuments({}, callback); // Pass an empty object as match condition to find all documents of this collection
-		}
+		},
+		category_count: function(callback) {
+			Category.countDocuments({}, callback); // Pass an empty object as match condition to find all documents of this collection
+		},
+		items_per_category_count: function(callback) {
+			Category.countDocuments({status:'Available'}, callback);
+		},
 	}, function(err, results) {
-		res.render('index', { title: 'Local Library Home', error: err, data: results });
+		res.render('index', { title: 'Store Overview', error: err, data: results });
 	});
+};
+
+exports.item_list = function(req, res, next) {
+	Item.find()
+		.exec(function(err, list_items) {
+			if (err) {
+				return next(err);
+			}
+			res.render('item_list', { title: 'Item List', item_list: list_items});
+		})
+}
+
+//display detail page for a specific author
+exports.item_detail = function(req, res, next) {
+	Item.findById(req.params.id)
+		.exec(function(err, item) {
+			if (err) {
+				return next(err);
+			}
+			res.render('item_detail', { title: 'Item Detail', item: item });
+		})
 };
--- routes/store.js
+++ routes/store.js
@@ -7,13 +7,13 @@ var category_controller = require('../controllers/categoryController');
 
 router.get('/', item_controller.index);
 
-// router.get('/items', item_controller.item_list);
+router.get('/items', item_controller.item_list);
 
 // router.get('/item/create', item_controller.item_create_get);
 
 // router.post('/item/create', item_controller.item_create_post);
 
-// router.get('/item/:id', item_controller.item_detail);
+router.get('/item/:id', item_controller.item_detail);
 
 // router.get('/item/delete', item_controller.item_delete_get);
 
