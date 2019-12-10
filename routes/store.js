6ce3113cec80a4fdebbbdd976aa910b749c398a8
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-12-10 16:08:08 -0500

    #5

--- routes/store.js
+++ routes/store.js
@@ -9,34 +9,34 @@ router.get('/', item_controller.index);
 
 router.get('/items', item_controller.item_list);
 
-// router.get('/item/create', item_controller.item_create_get);
+// router.get('/items/create', item_controller.item_create_get);
 
-// router.post('/item/create', item_controller.item_create_post);
+// router.post('/items/create', item_controller.item_create_post);
 
-router.get('/item/:id', item_controller.item_detail);
+router.get('/items/:id', item_controller.item_detail);
 
-// router.get('/item/:id/delete', item_controller.item_delete_get);
+// router.get('/items/:id/delete', item_controller.item_delete_get);
 
-// router.post('/item/:id/delete', item_controller.item_delete_post);
+// router.post('/items/:id/delete', item_controller.item_delete_post);
 
-// router.get('/item/:id/update', item_controller.item_update_get);
+// router.get('/items/:id/update', item_controller.item_update_get);
 
-// router.post('/item/:id/update', item_controller.item_update_post);
+// router.post('/items/:id/update', item_controller.item_update_post);
 
 router.get('/categories', category_controller.category_list);
 
-router.get('/category/create', category_controller.category_create_get);
+router.get('/categories/create', category_controller.category_create_get);
 
-router.post('/category/create', category_controller.category_create_post);
+router.post('/categories/create', category_controller.category_create_post);
 
-router.get('/category/:id', category_controller.category_detail);
+router.get('/categories/:id', category_controller.category_detail);
 
-router.get('/category/:id/delete', category_controller.category_delete_get);
+router.get('/categories/:id/delete', category_controller.category_delete_get);
 
-router.post('/category/:id/delete', category_controller.category_delete_post);
+router.post('/categories/:id/delete', category_controller.category_delete_post);
 
-router.get('/category/:id/update', category_controller.category_update_get);
+router.get('/categories/:id/update', category_controller.category_update_get);
 
-router.post('/category/:id/update', category_controller.category_update_post);
+router.post('/categories/:id/update', category_controller.category_update_post);
 
 module.exports = router;
