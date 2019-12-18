05408f33407e1577a2a3f094d9131375137c61d5
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-12-18 10:50:44 -0500

    https://github.com/pmocal/imaginaryinventory/issues/14

--- populatedb.js
+++ populatedb.js
@@ -1,23 +1,16 @@
 #! /usr/bin/env node
 
-console.log('This script populates your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true');
+console.log('This script populates your database. Database info provided by .env file. Change the string for database connection below if necessary for your db.');
+require('dotenv').config()
 
-// Get arguments passed on command line
-var userArgs = process.argv.slice(2);
-/*
-if (!userArgs[0].startsWith('mongodb')) {
-		console.log('ERROR: You need to specify a valid mongodb URL as the first argument');
-		return
-}
-*/
 var async = require('async')
 var Category = require('./models/category')
 var Item = require('./models/item')
 
 var mongoose = require('mongoose');
-const mongoDb = "mongodb+srv://" + process.env.DB_USER + ":" + 
+var mongoDb = "mongodb+srv://" + process.env.DB_USER + ":" + 
   process.env.DB_PASS + "@" + process.env.DB_HOST + "/express-auth?retryWrites=true&w=majority";
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(mongoDb, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
